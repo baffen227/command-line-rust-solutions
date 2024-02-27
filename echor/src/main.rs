d62b415826b648c7747466fe9abd1c3ac7137982
baffen227 <baffen227@gmail.com>
2024-02-27 16:32:57 +0800

    Create clap::App struct to parse command-line arguments

--- echor/src/main.rs
+++ echor/src/main.rs
@@ -1,3 +1,9 @@
+use clap::App;
+
 fn main() {
-    println!("{:?}", std::env::args());
+    let _matches = App::new("echor")
+        .version("0.1.0")
+        .author("Harry Chen <baffen227@gmail.com>")
+        .about("Rust echo")
+        .get_matches();
 }
